41d9ce1270b59673a49f0101b61c3c8de6b4255d
Juliana Marques <115050272+juliana-marques@users.noreply.github.com>
2023-07-26 09:48:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('unirio teste');
+  res.send('unirio oi teste');
 });
 
 app.get('*', (req, res) => {
